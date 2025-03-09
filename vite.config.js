28e707f583556378e6c1ba988a16af5c9656ee92
vladinSon <vladinson009@icloud.com>
2025-03-09 19:24:10 +0100

    #

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/host-tattoo/',
+  base: '/host-tattoo',
   plugins: [react(), tailwindcss()],
 });
