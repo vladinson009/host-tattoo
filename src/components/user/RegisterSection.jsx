7d8f6fde36cb99d3c78926268cc3de226b94ac12
vladinSon <vladinson009@icloud.com>
2025-03-09 17:32:26 +0100

    register section

--- src/components/user/RegisterSection.jsx
+++ src/components/user/RegisterSection.jsx
@@ -1,16 +1,57 @@
+import { useActionState, useContext } from "react"
+import userApi from "../../api/userApi";
+import { setUserData } from "../../utils/userSession";
+import { useNavigate } from "react-router-dom";
+import context from "../../context/context";
+
 export default function RegisterSection() {
+    const { setUserSession } = useContext(context)
+    const [{ error, userInput }, formAction, isPending] = useActionState(onRegister, { error: null, userInput: null });
+    const navigate = useNavigate()
+
+    async function onRegister(previousState, formData) {
+        const userInput = Object.fromEntries(formData);
+        try {
+            const user = await userApi.registerUser(userInput);
+            const userData = {
+                username: userInput.username,
+                email: userInput.email,
+                _id: user.objectId,
+                _token: user.sessionToken,
+            };
+
+            setUserData(userData);
+            setUserSession(userData)
+            navigate('/');
+            return user
+        } catch (error) {
+            return { error: error.message, userInput }
+        }
+    }
     return (
         <div className="opacity-75 min-h-screen flex flex-col items-center justify-center text-red-600 text-center p-6">
             <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
                 <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
-                <form>
+                {error && <p className="error">{error}</p>}
+                <form action={formAction}>
                     <div className="mb-4">
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            required
+                            defaultValue={userInput?.username}
+                            className="mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
+                        />
+                    </div>  <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
                         <input
                             type="email"
                             id="email"
                             name="email"
                             required
+                            defaultValue={userInput?.email}
                             className="mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
                         />
                     </div>
@@ -35,7 +76,7 @@ export default function RegisterSection() {
                         />
                     </div>
                     <div className="mb-6">
-                        <button type="submit" className="bg-black text-white hover:bg-red-600 hover:text-black font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out cursor-pointer bg-black text-white hover:bg-red-600 py-3 px-6 rounded-lg">
+                        <button disabled={isPending} type="submit" className="bg-black text-white hover:bg-red-600 hover:text-black font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out cursor-pointer bg-black text-white hover:bg-red-600 py-3 px-6 rounded-lg">
                             Register
                         </button>
                     </div>
