ca3e47640d94aa95e5789696b596774d48ff93ae
vladinSon <vladinson009@icloud.com>
2025-03-09 17:32:21 +0100

    logout functiopnality

---
+++ src/components/user/Logout.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom"
+import userApi from "../../api/userApi"
+import context from "../../context/context"
+import { useContext, useEffect } from "react"
+import { clearUserData } from "../../utils/userSession"
+export default function Logout() {
+    const { setUserSession } = useContext(context)
+
+    useEffect(() => {
+        userApi.logoutUser().then(() => {
+            clearUserData();
+            setUserSession(null);
+        })
+    }, [setUserSession])
+    return (
+        <Navigate to={'/'} />
+    )
+}
