18bc279e248219392d1bc6123e1cd2c08aba06cb
vladinSon <vladinson009@icloud.com>
2025-03-09 18:28:46 +0100

    #

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './App.css'
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/host-tattoo">
     <App />
   </BrowserRouter>
   // {/* </StrictMode>, */}
