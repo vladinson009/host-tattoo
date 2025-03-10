3d843bc60c05da29fe61a4ac29f74ff95c0281a1
vladinSon <vladinson009@icloud.com>
2025-03-10 01:24:26 +0100

    gallery section

--- src/components/gallery/GallerySection.jsx
+++ src/components/gallery/GallerySection.jsx
@@ -1,34 +1,36 @@
 import { useEffect, useState } from "react";
+import postApi from "../../api/postApi";
+import galleryApi from "../../api/galleryApi";
 
 export default function GallerySection() {
-    const [images, setImages] = useState([]);
+    const [posts, setPosts] = useState([]);
 
     // Could be GET or POST/PUT/PATCH/DELETE
     useEffect(() => {
-        fetch('https://dummyjson.com/recipes')
-            .then(res => res.json())
-            .then(result => setImages(result.recipes)
-            );
+        galleryApi.getGallery(10, 0).then((res) => {
+            setPosts(res.results);
+
+        })
     }, [])
 
     /* { status: 'ok', method: 'GET' } */
 
     return (
-        <section className="slide-enter-active min-h-screen text-white py-16">
+        <section className="slide-enter-active min-h-screen  text-white py-16">
             <div className="container mx-auto px-6">
-                <h2 className="text-4xl font-black gothic mb-12 drop-shadow-lg text-center pt-16">Tattoo Gallery</h2>
+                <h2 className="text-4xl font-black gothic mb-12 drop-shadow-lg text-center pt-16">Our Gallery</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-6">
-                    {images.map((image, index) => (
-                        <div key={index} className="relative group w-72 h-96 bg-gray-900 rounded-2xl overflow-hidden shadow-xl border border-red-800">
+                    {posts.map((post) => (
+                        <div key={post.objectId} className="relative group w-72 h-96 bg-gray-900 rounded-2xl overflow-hidden shadow-xl border border-red-800">
                             <img
-                                src={image.image}
-                                alt={image.name}
+                                src={post.image.url}
+                                alt={post.title}
                                 className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-105"
                             />
                             <div className="absolute inset-0 bg-black opacity-50 group-hover:opacity-0 transition-opacity duration-500"></div>
                             <div className="p-4 flex flex-col items-center">
-                                <h3 className="text-2xl font-bold gothic text-white">Name</h3>
-                                <p className="text-sm text-gray-300 text-center mt-2">Descripition</p>
+                                <h3 className="text-2xl font-bold gothic text-white">{post.title}</h3>
+                                <p className="text-sm text-gray-300 text-center mt-2">{post.description.length > 65 ? post.description.slice(0, 60) + '...' : post.description}</p>
                             </div>
                         </div>
                     ))}
