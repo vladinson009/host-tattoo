bd814e811b2cd5ed96287f76ff086737a0fbca1a
vladinSon <vladinson009@icloud.com>
2025-03-15 04:03:26 +0100

    fixed Routes

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ArtistsSection from "./components/artist/ArtistsSection";
 import Logout from "./components/user/Logout";
 import CreatePost from "./components/gallery/CreatePost";
 import NewsFeed from "./components/news-feed/NewsFeed";
+import ArtistDetails from "./components/artist/ArtistDetails";
 
 function App() {
   const location = useLocation();
@@ -24,36 +25,41 @@ function App() {
   return (
     <ContextProvider>
       <Header />
-      <Suspense fallback={<div className="bg-black">Loading...</div>}>
-        <TransitionGroup>
-          <CSSTransition
-            key={location.pathname}
-            timeout={300}
-            classNames="bounce"
-            unmountOnExit
-            nodeRef={nodeRef}
-          >
-            <div ref={nodeRef}>
-              <Routes location={location}>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LoginSection />} />
-                <Route path="/register" element={<RegisterSection />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/contact" element={<ContactSectioin />} />
-                <Route path="/artists" element={<ArtistsSection />} />
-                <Route path="/create-post" element={<CreatePost />} />
-                <Route path="/news-feed" element={<NewsFeed />} />
 
-                <Route path="/gallery">
-                  <Route path="" element={<GallerySection />} />
-                  <Route path="details/:tattooId" element={<DetailsSection />} />
-                </Route>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.pathname}
+          timeout={300}
+          classNames="bounce"
+          unmountOnExit
+          nodeRef={nodeRef}
+        >
+          <div style={{ minHeight: 'calc(100vh - 3.8rem)' }} ref={nodeRef}>
+            <Routes location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginSection />} />
+              <Route path="/register" element={<RegisterSection />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/contact" element={<ContactSectioin />} />
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/news-feed" element={<NewsFeed />} />
 
-              </Routes>
-            </div>
-          </CSSTransition>
-        </TransitionGroup>
-      </Suspense>
+              <Route path='/artists'>
+                <Route index element={<ArtistsSection />} />
+                <Route path=":artistId" element={<ArtistDetails />} />
+
+              </Route>
+
+              <Route path="/gallery">
+                <Route path="" element={<GallerySection />} />
+                <Route path="details/:tattooId" element={<DetailsSection />} />
+              </Route>
+
+            </Routes>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+
       <Footer />
       <Analytics />
     </ContextProvider>
