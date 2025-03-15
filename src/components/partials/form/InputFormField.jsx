8988f8a3957b51ed704f5828bcf2c77943fffeb0
vladinSon <vladinson009@icloud.com>
2025-03-15 05:20:08 +0100

    reusable input form field

--- src/components/partials/form/InputFormField.jsx
+++ src/components/partials/form/InputFormField.jsx
@@ -2,14 +2,14 @@ export default function InputFormField({ labelName, type, name, value }) {
 
     return (
         <div className="mb-4">
-            <label htmlFor={name} className="block text-sm font-medium text-gray-700">{labelName}</label>
+            <label htmlFor={name} className="text-2xl text-white">{labelName}</label>
             <input
                 type={type}
                 id={name}
                 name={name}
                 required
                 defaultValue={value}
-                className="mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
+                className="text-2xl md:text-3xl mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
             />
         </div>
     )
