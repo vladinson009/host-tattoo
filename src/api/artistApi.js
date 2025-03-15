ff783275b440a3ca39da46eb9104096424987d72
vladinSon <vladinson009@icloud.com>
2025-03-15 04:04:12 +0100

    implement artist functionality

--- src/api/artistApi.js
+++ src/api/artistApi.js
@@ -1,19 +1,43 @@
 import { get, put } from './fetcher';
 
-async function getAllArtists() {
-  const { results } = await get('/classes/Artist');
+async function getAllArtists(signal) {
+  const { results } = await get('/classes/Artist', signal);
   return results;
 }
-async function addLikeToArtist(artistId, currentUserId) {
+async function getArtistById(artistId, signal) {
+  return get('/classes/Artist/' + artistId, signal);
+}
+async function addLikeToArtist(artistId, currentUserId, signal) {
   const body = {
     likes: {
       __op: 'AddUnique',
       objects: [currentUserId],
     },
   };
-  return put(`/classes/Artist/${artistId}`, body);
+  return put(`/classes/Artist/${artistId}`, body, signal);
+}
+async function removeLikeFromArtist(artistId, currentUserId, signal) {
+  const body = {
+    likes: {
+      __op: 'Remove',
+      objects: [currentUserId],
+    },
+  };
+  return put(`/classes/Artist/${artistId}`, body, signal);
+}
+async function getLikes(userIds, signal) {
+  const result = await get(
+    `/classes/_User?where=${encodeURIComponent(
+      JSON.stringify({ objectId: { $in: userIds } })
+    )}`,
+    signal
+  );
+  return result.results;
 }
 export default {
   getAllArtists,
+  getArtistById,
   addLikeToArtist,
+  removeLikeFromArtist,
+  getLikes,
 };
