a0b1821bcecbb2a500b22f940311f08720e499cc
vladinSon <vladinson009@icloud.com>
2025-03-15 04:03:47 +0100

    work in strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
 import './App.css'
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // {/* </StrictMode>, */}
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 )
