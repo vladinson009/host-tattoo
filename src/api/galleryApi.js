ab840ae04be43c4ae6db64dcc00e920e278bd76d
vladinSon <vladinson009@icloud.com>
2025-03-15 04:05:31 +0100

    get tattos by artistId api function

--- src/api/galleryApi.js
+++ src/api/galleryApi.js
@@ -1,16 +1,32 @@
 import { get, post } from './fetcher';
 
-async function getGallery(limit, skip) {
+async function getGallery(limit, skip, signal) {
   const gallery = await get(
-    `/classes/Gallery?limit=${limit}&skip=${skip}&order=-createdAt`
+    `/classes/Gallery?limit=${limit}&skip=${skip}&order=-createdAt&include=artistId`,
+    signal
   );
   return gallery.results;
 }
-function createGalleryPost(data) {
-  return post('/classes/Gallery', data);
+function createGalleryPost(data, signal) {
+  return post('/classes/Gallery', data, signal);
 }
-
+async function getTattoosByArtistId(artistId, signal) {
+  const { results } = await get(
+    `/classes/Gallery?where=${encodeURIComponent(
+      JSON.stringify({
+        artistId: {
+          __type: 'Pointer',
+          className: 'Artist',
+          objectId: artistId, // Assuming artistId is the objectId of the artist
+        },
+      })
+    )}&order=-createdAt`,
+    signal
+  );
+  return results;
+}
 export default {
   getGallery,
   createGalleryPost,
+  getTattoosByArtistId,
 };
