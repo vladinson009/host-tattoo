8c850c952ab25e4aed1087363ebce5726e03c5be
vladinSon <vladinson009@icloud.com>
2025-03-15 05:19:46 +0100

    form footer

--- src/components/partials/form/FormFooter.jsx
+++ src/components/partials/form/FormFooter.jsx
@@ -4,7 +4,7 @@ export default function FormFooter({ text, linkText, link }) {
 
     return (
         <div className="text-center">
-            <p className="text-sm">
+            <p className="text-lg">
                 {text} <Link to={link} className="text-red-600 font-semibold hover:text-red-800 hover:underline hover:shadow-md transition-all duration-300 ease-in-out">{linkText}</Link>
             </p>
         </div>
