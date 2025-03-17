e178ce07349e4a1e763afdfd9e4f88f6b105df42
vladinSon <vladinson009@icloud.com>
2025-03-17 22:23:26 +0100

    use useFetchData hook

--- src/components/gallery/GallerySection.jsx
+++ src/components/gallery/GallerySection.jsx
@@ -1,24 +1,22 @@
-import { useEffect, useState } from "react";
-import galleryApi from "../../api/galleryApi";
+import { useContext, useState } from "react";
 import TattooCard from "./TattooCard";
-
+import Spinner from "../partials/Spinner";
+import useFetchData from "../../hooks/useFetchData";
+import { fetchGallery } from "./galleryUtils";
+import context from "../../context/context";
 export default function GallerySection() {
-    const [posts, setPosts] = useState([]);
+    const { userSession } = useContext(context)
+    const [tattoos, setTattoos] = useState([]);
+    const [, isLoading] = useFetchData(fetchGallery, setTattoos, userSession?._id)
 
-    useEffect(() => {
-        const controller = new AbortController()
-        galleryApi.getGallery(10, 0, controller.signal).then((setPosts))
-        return () => controller.abort();
-    }, [])
-
-
+    if (isLoading) return <Spinner />
     return (
         <section className="min-h-[calc(100vh-4rem-4rem)] text-white py-16 ">
             <div className="container mx-auto px-6 ">
                 <h2 className="text-4xl font-black mb-12 drop-shadow-lg text-center pt-16">Our Gallery</h2>
                 <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]  place-items-center gap-20 md:gap-25 lg:gap-30 px-6">
-                    {posts.map((post) => (
-                        <TattooCard key={post.objectId} post={post} />
+                    {tattoos.map((tattoo) => (
+                        <TattooCard key={tattoo.objectId} tattoo={tattoo} setTattoos={setTattoos} />
                     ))}
                 </div>
             </div>
