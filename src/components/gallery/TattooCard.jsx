8b532e48bf246ab0c919b614da0826784e7bfbd3
vladinSon <vladinson009@icloud.com>
2025-03-17 22:24:18 +0100

    added functionality on tattoo card

--- src/components/gallery/TattooCard.jsx
+++ src/components/gallery/TattooCard.jsx
@@ -1,33 +1,55 @@
 import { Link } from "react-router";
+import { GiEvilLove } from "react-icons/gi";
+import { useContext } from "react";
+import context from "../../context/context";
+import { onWishlist, onUnwishlist } from "./galleryUtils";
 
-export default function TattooCard({ post }) {
+export default function TattooCard({ tattoo, setTattoos }) {
+    const { userSession } = useContext(context);
+
+
+
 
     return (
         <div className="relative group w-90 h-full bg-[rgba(31,41,55,0.8)] rounded-2xl overflow-hidden shadow-xl border border-red-800 flex flex-col ">
             {/* Image Section */}
-            <div className="cursor-pointer w-full h-auto overflow-hidden rounded-lg">
+
+            <Link to={`/gallery/details/${tattoo.objectId}`} className="cursor-pointer w-full h-auto overflow-hidden rounded-lg">
                 <img
-                    src={post.image.url}
-                    alt={post.title}
+                    src={tattoo.image.url}
+                    alt={tattoo.title}
                     className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                 />
-            </div>
+            </Link>
 
             {/* Content Section */}
             <div className="text-center px-2">
-                <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white">{post.title}</h3>
+                <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white">{tattoo.title}</h3>
                 <p className="text-xl sm:text-xl md:text-1xl lg:text-2xl text-gray-400 mt-2">
-                    {post.description}
+                    {tattoo.description}
                 </p>
             </div>
 
             {/* Artist Info */}
-            {post.artistId.name && <p className="text-white font-medium m-4 bottom">
+            {tattoo.artistId.name && <p className="text-white font-medium m-4 bottom">
                 Posted by:
-                <Link to={`/artists/${post.artistId.objectId}`}>
-                    <span className="text-red-600 font-bold text-3xl sm:text-3xl md:text-4xl cursor-pointer hover:text-yellow-500 hover:scale-110 hover:shadow-lg transition-all duration-300"> {post.artistId.name}</span>
+                <Link to={`/artists/${tattoo.artistId.objectId}`}>
+                    <span className="text-red-600 font-bold text-3xl sm:text-3xl md:text-4xl cursor-pointer hover:text-yellow-500 hover:scale-110 hover:shadow-lg transition-all duration-300"> {tattoo.artistId.name}</span>
                 </Link>
             </p>}
+
+            {userSession && <div className="mt-auto mb-4 text-center">
+                {tattoo.isWishlist
+                    ? <button onClick={onUnwishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-red-600 cursor-pointer hover:text-gray-400 transition duration-300">
+                        Remove favorite
+                        < GiEvilLove />
+                    </button>
+                    : <button onClick={onWishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-gray-400 cursor-pointer hover:text-red-600 transition duration-300">
+                        Add favorite
+                        < GiEvilLove />
+                    </button>
+                }
+            </div>}
         </div>
     )
 }
