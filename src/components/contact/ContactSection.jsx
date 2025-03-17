efb507255bf7b759f3519ad256e321b0e8659d12
vladinSon <vladinson009@icloud.com>
2025-03-17 22:21:57 +0100

    added spinner

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -1,4 +1,4 @@
-import contactApi from "../../api/contactApi";
+import contactService from "../../services/contactService";
 
 import useCreateForm from "../../hooks/useCreateForm";
 
@@ -9,20 +9,23 @@ import TextareaFormField from "../partials/form/textareaFormField";
 import SubmitFormButton from "../partials/form/SubmitFormButton";
 import FormFooter from "../partials/form/FormFooter";
 import { useEffect, useState } from "react";
-import artistApi from "../../api/artistApi";
+import artistService from "../../services/artistService";
+import Spinner from "../partials/Spinner";
 
 export default function ContactSection() {
     const [artists, setArtists] = useState([]);
-    const { formAction, isPending, error, userInput } = useCreateForm(contactApi.contactUsForm)
+    const { formAction, isPending, error, userInput } = useCreateForm(contactService.contactUsForm)
 
     useEffect(() => {
         const controller = new AbortController();
         (async () => {
-            const data = await artistApi.getAllArtists(controller.signal);
+            const data = await artistService.getAllArtists(controller.signal);
             setArtists(data)
         })()
         return () => controller.abort()
     }, [])
+
+    if (isPending) return <Spinner />
     return (
         <Form name="Contact Us" error={error} action={formAction} >
             <SelectFormField labelName="Choose artist" name="artistId" artists={artists} />
