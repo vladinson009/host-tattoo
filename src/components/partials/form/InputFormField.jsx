3f8c844a46ff0cc8cc3bf15cbd04cf1ec13767c7
vladinSon <vladinson009@icloud.com>
2025-03-18 21:47:38 +0100

    reduce text size

--- src/components/partials/form/InputFormField.jsx
+++ src/components/partials/form/InputFormField.jsx
@@ -9,7 +9,7 @@ export default function InputFormField({ labelName, type, name, value }) {
                 name={name}
                 required
                 defaultValue={value}
-                className="text-2xl md:text-3xl mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
+                className="text-1xl md:text-2xl mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
             />
         </div>
     )
--- src/components/partials/form/TextareaFormField.jsx
+++ src/components/partials/form/TextareaFormField.jsx
@@ -8,7 +8,7 @@ export default function TextareaFormField({ name, labelName, value }) {
                 name={name}
                 required
                 defaultValue={value}
-                className="text-2xl md:text-3xl mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500 resize-none"
+                className="text-1xl md:text-1xl mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500 resize-none"
             />
         </div>
     )
