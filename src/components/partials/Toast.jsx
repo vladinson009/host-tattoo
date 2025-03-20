8b21bfd7036f1fa28a98379db464b23297376304
vladinSon <vladinson009@icloud.com>
2025-03-21 00:34:19 +0100

    added type color for Toast

--- src/components/partials/Toast.jsx
+++ src/components/partials/Toast.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-export default function Toast({ message }) {
+export default function Toast({ message, type }) {
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
@@ -12,18 +12,26 @@ export default function Toast({ message }) {
         return () => clearTimeout(timer);
     }, [])
 
+    function typeColor(type) {
+        if (type == 'message') {
+            return 'bg-green-600'
+        } else {
+            return 'bg-red-600'
+        }
+    }
     return (
         isOpen && <motion.div
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 100 }}
             transition={{ duration: 0.5 }}
-            className={`z-50 fixed top-1/7 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-xs p-4 mb-4 rounded-lg shadow-lg text-white bg-red-600`}
+            className={`z-950 fixed top-1/7 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-xs p-4 mb-4 rounded-lg shadow-lg text-white ${typeColor(type)}`}
             role="alert"
             onClick={() => setIsOpen(false)}
+
         >
             <div className="flex items-center space-x-3">
-                <span>{message}</span>
+                <span className="text-center">{message}</span>
             </div>
         </motion.div>
     );
