496acb629d9a5a75acee88ad420c2edb59494bd2
vladinSon <vladinson009@icloud.com>
2025-03-21 00:36:51 +0100

    added global message in context providera

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -5,8 +5,17 @@ import { getUserSession } from "../utils/userSession";
 export default function ContextProvider({ children }) {
     const { Provider } = context;
     const [userSession, setUserSession] = useState(getUserSession());
+    const [globalMessage, setGlobalMessage] = useState(false);
+
+    function tempMessage(message) {
+        setGlobalMessage(message);
+        setTimeout(() => {
+            setGlobalMessage(false);
+
+        }, 6000);
+    }
     return (
-        <Provider value={{ userSession, setUserSession }}>
+        <Provider value={{ userSession, setUserSession, globalMessage, tempMessage }}>
             {children}
         </Provider>
     )
