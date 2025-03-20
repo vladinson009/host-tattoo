859063a50f823db961bf0ddf38c5ef8868bc6d9b
vladinSon <vladinson009@icloud.com>
2025-03-21 00:34:41 +0100

    added archiving function to messages

--- src/components/partials/message/messageCard.jsx
+++ src/components/partials/message/messageCard.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { X } from 'lucide-react';
+import { useState } from 'react';
+import artistService from '../../../services/artistService';
+import { GiToaster } from 'react-icons/gi';
+import Toast from '../Toast';
+import Spinner from '../Spinner';
 
 function MessageCard({ messageData }) {
     const { message, topic, customerId, artistId, createdAt } = messageData;
     const { objectId } = customerId;
-
+    const [isArchived, setIsArchived] = useState(false);
+    const [error, setError] = useState('')
+    async function onArchive() {
+        try {
+            await artistService.archiveMessageById(messageData.objectId)
+            setIsArchived(true);
+        } catch (error) {
+            setError(error.message);
+        }
+    }
     return (
         <div className="max-w-3xl mx-auto bg-gray-800 text-white rounded-lg shadow-lg mb-6 p-6">
+            {error && <Toast message={error} type="message" />}
+            <div className="relative left-[calc(100%-2.5rem)] ">
+                {isArchived ? <p>Archived</p> : <X onClick={onArchive} className='cursor-pointer hover:text-red-500' />}
+            </div>
             <div className="flex items-center space-x-4">
                 <div>
                     <h2 className="text-xl font-semibold text-red-600">User ID: {objectId}</h2>
