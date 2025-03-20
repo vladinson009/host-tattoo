cd0fc57b8d4bacd4074acf50754deef4e0cdb60e
vladinSon <vladinson009@icloud.com>
2025-03-20 01:00:47 +0100

    renamed contact form to booking section

--- src/components/booking/BookingSection.jsx
+++ src/components/booking/BookingSection.jsx
@@ -11,8 +11,9 @@ import FormFooter from "../partials/form/FormFooter";
 import { useEffect, useState } from "react";
 import artistService from "../../services/artistService";
 import Spinner from "../partials/Spinner";
+import Toast from "../partials/Toast";
 
-export default function ContactSection() {
+export default function BookingSection() {
     const [artists, setArtists] = useState([]);
     const { formAction, isPending, error, userInput } = useCreateForm(contactService.contactUsForm)
 
@@ -27,12 +28,15 @@ export default function ContactSection() {
 
     if (isPending) return <Spinner />
     return (
-        <Form name="Contact Us" error={error} action={formAction} >
-            <SelectFormField labelName="Choose artist" name="artistId" artists={artists} />
-            <InputFormField labelName='Topic' type='text' name='topic' value={userInput?.topic} />
-            <TextareaFormField labelName='Message' name='message' value={userInput?.message} />
-            <SubmitFormButton isPending={isPending} textContent='Send email' />
-            <FormFooter text="Wondering which artist to ask?" link='/artists' linkText='Check our artists' />
-        </Form>
+        <>
+            {error && <Toast message={error} />}
+            <Form name="Make Booking" action={formAction} >
+                <SelectFormField labelName="Choose artist" name="artistId" artists={artists} />
+                <InputFormField labelName='Topic' type='text' name='topic' value={userInput?.topic} />
+                <TextareaFormField labelName='Short message...' name='message' value={userInput?.message} />
+                <SubmitFormButton isPending={isPending} textContent='Send email' />
+                <FormFooter text="Wondering which artist to choose?" link='/artists' linkText='Check our artists' />
+            </Form>
+        </>
     )
 }
