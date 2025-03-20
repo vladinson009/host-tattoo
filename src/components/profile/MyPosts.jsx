485845cc4a9454c2e3d93b92737942d0a3deac26
vladinSon <vladinson009@icloud.com>
2025-03-21 00:36:14 +0100

    bug fixed with pagination

--- src/components/profile/MyPosts.jsx
+++ src/components/profile/MyPosts.jsx
@@ -3,7 +3,7 @@ import PostComponent from "../post/PostComponent";
 import postService from '../../services/postService';
 
 async function fetchOwnPosts(setPost, _id, signal) {
-    const posts = await postService.getOwnPosts(100, 0, signal);
+    const posts = await postService.getOwnPosts(signal);
     const updatedPosts = posts.map((post) => {
         return { ...post, isLiked: post?.likes?.includes(_id) };
     });
