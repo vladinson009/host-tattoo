1b4867afd8255028587b9d4d281a9d244f427d4e
vladinSon <vladinson009@icloud.com>
2025-03-21 00:38:06 +0100

    added global  message when make post request with form

--- src/hooks/useCreateForm.jsx
+++ src/hooks/useCreateForm.jsx
@@ -1,7 +1,9 @@
-import { useActionState } from "react";
+import { useActionState, useContext } from "react";
 import { useNavigate } from "react-router";
+import context from "../context/context";
 
 export default function useCreateForm(apiCall) {
+    const { tempMessage } = useContext(context);
     const navigate = useNavigate();
     const [{ userInput = {}, error }, formAction, isPending] = useActionState(onAction, { userInput: null, error: null })
 
@@ -10,6 +12,7 @@ export default function useCreateForm(apiCall) {
         try {
             await apiCall(formData);
             navigate('/news-feed');
+            tempMessage("Created")
             return userInput
         } catch (error) {
             return { error: error.message, userInput }
