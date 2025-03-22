22b4868fc5fd98bc2b952279ff25f90464073ab0
vladinSon <vladinson009@icloud.com>
2025-03-22 16:02:31 +0100

    moved buttons in own compnent with custom hook

--- src/components/gallery/TattooCard.jsx
+++ src/components/gallery/TattooCard.jsx
@@ -1,63 +1,27 @@
 import { Link } from "react-router";
-import { GiEvilLove } from "react-icons/gi";
-import { useContext } from "react";
-import context from "../../context/context";
-import { onWishlist, onUnwishlist } from "./galleryUtils";
 
-export default function TattooCard({ tattoo, setTattoos }) {
-    const { userSession } = useContext(context);
-
-
-
+export default function TattooCard({ tattoo }) {
     return (
         <div className="relative group w-90 h-full bg-[rgba(31,41,55,0.8)] rounded-2xl overflow-hidden shadow-xl border border-red-800 flex flex-col ">
-            {/* Image Section */}
-
             <Link to={`/gallery/details/${tattoo.objectId}`} className="cursor-pointer w-full h-auto overflow-hidden rounded-lg">
                 <img
                     src={tattoo.image.url}
                     alt={tattoo.title}
-                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+                    className="w-full h-[300px] object-cover transition-transform duration-500 group-hover:scale-105"
                 />
             </Link>
-
-            {/* Content Section */}
             <div className="text-center px-2">
                 <h3 className="text-xl sm:text-1xl md:text-2xl lg:text-3xl text-white">{tattoo.title}</h3>
-                <p className="text-xl sm:text-xl md:text-1xl lg:text-2xl text-gray-400 mt-2">
+                {/* <p className="text-xl sm:text-xl md:text-1xl lg:text-2xl text-gray-400 mt-2">
                     {tattoo.description}
-                </p>
+                </p> */}
             </div>
-
-            {/* Artist Info */}
             <p className="text-white font-medium m-4 bottom">
                 Posted by:
                 <Link to={`/artists/${tattoo.artistId.objectId}`}>
                     <span className="text-red-600 text-1xl sm:text-2xl md:text-3xl cursor-pointer hover:text-yellow-500 hover:scale-110 hover:shadow-lg transition-all duration-300"> {tattoo.artistId.name}</span>
                 </Link>
             </p>
-
-            {userSession && tattoo.artistId.artistId !== userSession._id ?
-                <div className="mt-auto mb-4 text-center">
-                    {tattoo.isWishlist
-                        ? <button onClick={onUnwishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-red-600 cursor-pointer hover:text-gray-400 transition duration-300">
-                            Remove from wishlist
-                            < GiEvilLove />
-                        </button>
-                        : <button onClick={onWishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-gray-400 cursor-pointer hover:text-red-600 transition duration-300">
-                            Add to wishlist
-                            < GiEvilLove />
-                        </button>
-                    }  </div> : userSession && tattoo.artistId.artistId == userSession._id ? <div className="mt-auto mb-4 text-center" >
-                        <button onClick={onWishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-gray-400 cursor-pointer hover:text-red-600 transition duration-300">
-                            Delete
-                            < GiEvilLove />
-                        </button>
-                        <button onClick={onWishlist.bind(tattoo, setTattoos)} className="inline-flex items-center ml-2  text-2xl sm:text-2xl md:text-3xl text-gray-400 cursor-pointer hover:text-red-600 transition duration-300">
-                            Edit
-                            < GiEvilLove />
-                        </button>
-                    </div> : null}
         </div>
     )
 }
