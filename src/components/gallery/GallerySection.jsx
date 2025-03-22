6ee0872bf848bd5836a3260e94f2ace5bd26fd30
vladinSon <vladinson009@icloud.com>
2025-03-22 16:01:19 +0100

    redo gallery section in own component

--- src/components/gallery/GallerySection.jsx
+++ src/components/gallery/GallerySection.jsx
@@ -1,7 +1,29 @@
-import TattoosComponent from "./TattoosComponent";
-import { fetchGallery } from "./galleryUtils";
+import useFetchData from "../../hooks/useFetchData";
 
+import TattooCard from "./TattooCard";
+import Spinner from "../partials/Spinner";
+import Toast from "../partials/Toast";
+import NoContent from "../partials/NoContent";
+import galleryService from "../../services/galleryService";
 
 export default function GallerySection() {
-    return <TattoosComponent title={'Our Tattoo Gallery'} fetchData={fetchGallery} />
+    const { data, isLoading, error } = useFetchData(galleryService.getGallery)
+
+    if (isLoading) return <Spinner />
+    return (
+        <>
+            {error && <Toast message={error} />}
+            <section className="min-h-[calc(100vh-4rem-4rem)] text-white py-16 ">
+                <div className="container mx-auto px-6 ">
+                    <h2 className="text-4xl font-black mb-12 drop-shadow-lg text-center pt-16">Our Tattoo Gallery</h2>
+                    {data.length > 0 ? <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]  place-items-center gap-20 md:gap-25 lg:gap-30 px-6">
+                        {data.map((tattoo) => (
+                            <TattooCard key={tattoo.objectId} tattoo={tattoo} />
+                        ))}
+                    </div>
+                        : <NoContent content="Nothing in Gallery yet..." />}
+                </div>
+            </section>
+        </>
+    )
 }
