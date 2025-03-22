f9b24887838dd9047da44b27afa4cdd365f432d9
vladinSon <vladinson009@icloud.com>
2025-03-22 16:09:05 +0100

    hook for details tattoo section showing if item is in wishlist

---
+++ src/hooks/useDetailsTattooSection.jsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import context from "../context/context";
+import { useNavigate } from "react-router";
+import galleryService from "../services/galleryService";
+
+export default function useDetailsTattooSection(tattooId) {
+
+    const navigate = useNavigate();
+    const [tattoo, setTattoo] = useState();
+    const { userSession } = useContext(context);
+    useEffect(() => {
+        const controller = new AbortController();
+        (async () => {
+            const [data, wishlistId] = await Promise.all([
+                galleryService.getTattooById(tattooId, controller.signal),
+                galleryService.retrieveWishlistId(userSession?._id, tattooId)
+            ])
+
+            setTattoo({ ...data, isWishlist: wishlistId })
+        })()
+        return () => controller.abort();
+    }, [tattooId, userSession?._id]);
+
+    return { tattoo, navigate, setTattoo }
+}
