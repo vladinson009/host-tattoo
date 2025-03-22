0d27f917ef9978f6532d173bcd2017af8dec6120
vladinSon <vladinson009@icloud.com>
2025-03-22 16:05:08 +0100

    moved non reusable util function in component

--- src/components/post/NewsFeed.jsx
+++ src/components/post/NewsFeed.jsx
@@ -1,5 +1,13 @@
-import { fetchPosts } from "./postUtils";
+import postService from "../../services/postService";
 import PostComponent from "./PostComponent";
+
+async function fetchPosts(setPost, _id, signal) {
+    const fetchPosts = await postService.getPosts(signal);
+    const updatedArtists = fetchPosts.map((post) => {
+        return { ...post, isLiked: post?.likes?.includes(_id) };
+    });
+    setPost(updatedArtists);
+}
 
 export default function NewsFeed() {
     return (<PostComponent fetchPosts={fetchPosts} title="News Feed" />);
