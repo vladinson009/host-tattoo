edd51dcae8f86e102de73895da31fb712853dc78
vladinSon <vladinson009@icloud.com>
2025-03-25 23:33:14 +0100

    integrated react query in the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App.jsx'
 import './App.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 6000,
+      refetchInterval: 60000,
+      cacheTime: 4000,
+      retry: 3,
+      gcTime: 10000,
+    }
+  }
+})
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // {/* </StrictMode>, */}
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
 )
