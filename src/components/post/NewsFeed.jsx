66105484e214ac3c4c99b869dd78408ded804500
vladinSon <vladinson009@icloud.com>
2025-03-25 23:39:22 +0100

    implemented useQuery in fetching news feed

--- src/components/post/NewsFeed.jsx
+++ src/components/post/NewsFeed.jsx
@@ -1,16 +1,28 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import postService from "../../services/postService";
 import PostComponent from "./PostComponent";
+import { useContext } from "react";
+import context from "../../context/context";
 
-async function fetchPosts(setPost, _id, signal) {
-    const fetchPosts = await postService.getPosts(signal);
-    const updatedArtists = fetchPosts.map((post) => {
-        return { ...post, isLiked: post?.likes?.includes(_id) };
-    });
-    setPost(updatedArtists);
-}
 
 export default function NewsFeed() {
+    const queryClient = useQueryClient();
+    const { userSession } = useContext(context);
 
-    // show all posts in News Feed section
-    return (<PostComponent fetchPosts={fetchPosts} title="News Feed" />);
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['fetchPosts'],
+        queryFn: async () => {
+            const result = await postService.getPosts();
+            const updatedArtists = result.map((post) => {
+                return { ...post, isLiked: post?.likes?.includes(userSession?._id) };
+            });
+            return updatedArtists;
+        },
+        onSuccess: (data) => {
+            queryClient.setQueryData(['fetchPosts'], data);
+        }, onError: (error) => {
+            console.log(error);
+        }
+    })
+    return (<PostComponent data={data} error={error} isLoading={isLoading} title="News Feed" />);
 }
