c1c662a01f8a5fadb78c293f14bae9abfe5b50f1
vladinSon <vladinson009@icloud.com>
2025-03-25 23:40:19 +0100

    improve props drilling

--- src/components/post/postCard.jsx
+++ src/components/post/postCard.jsx
@@ -11,17 +11,18 @@ import EditModal from "../modals/EditModal";
 import usePostCard from "../../hooks/usePostCard";
 import Toast from "../partials/Toast";
 
-export default function PostCard({ post, setPost, userSession }) {
+export default function PostCard({ post, userSession }) {
+    const [currentPost, setCurrentPost] = useState(post);
     const [commentsCount, setCommentsCount] = useState(0)
     const [isCommentModalOpen, setIsCommentModalOpen] = useState(false);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const { isOwner, isPending, onEdit, onDelete, onLike, onUnlike, error } = usePostCard(post, setPost, userSession)
+    const { isOwner, isPending, onEdit, onDelete, onLike, onUnlike, error } = usePostCard(currentPost, setCurrentPost, userSession)
 
 
     return (
         <motion.div
-            key={post.objectId}
+            key={currentPost.objectId}
             className="bg-[rgba(31,41,55,0.8)] p-4 rounded-2xl shadow-lg border border-gray-800"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -30,30 +31,30 @@ export default function PostCard({ post, setPost, userSession }) {
             {error && <Toast message={error} type="error" />}
             <div className="flex items-center space-x-4">
                 <div>
-                    <img src={post.image.url} alt={post.title} className="rounded-full" />
+                    <img src={currentPost.image.url} alt={currentPost.title} className="rounded-full" />
                 </div>
                 <div>
-                    <h3 className="text-white text-2xl sm:text-2xl md:text-4xl">{post.title}</h3>
+                    <h3 className="text-white text-2xl sm:text-2xl md:text-4xl">{currentPost.title}</h3>
                     <p className="text-1xl sm:text-1xl md:text-1xl text-gray-400">
-                        {formatDistanceToNow(new Date(post.createdAt))} ago
+                        {formatDistanceToNow(new Date(currentPost.createdAt))} ago
                     </p>
                 </div>
             </div>
             <div className="flex items-center justify-between mt-4"></div>
-            <p className="text-white mt-2">Posted by: <span className="text-red-600 text-1xl sm:text-2xl md:text-3xl">{post.owner}</span></p>
+            <p className="text-white mt-2">Posted by: <span className="text-red-600 text-1xl sm:text-2xl md:text-3xl">{currentPost.owner}</span></p>
             <div className="mt-4 text-gray-300">
-                <p className="leading-8 sm:leading-9 md:leading-10 text-1xl sm:text-1xl md:text-2xl">{post.description}</p>
+                <p className="leading-8 sm:leading-9 md:leading-10 text-1xl sm:text-1xl md:text-2xl">{currentPost.description}</p>
             </div>
             {userSession && <div className="flex space-x-4 items-center justify-evenly mt-4">
                 <div>
                     <button disabled={isPending} className="text-4xl sm:text-5xl md:text-6xl text-gray-400 cursor-pointer hover:text-red-500">
                         {
-                            post.isLiked
+                            currentPost.isLiked
                                 ? <FaHeart onClick={onUnlike.bind(post)} className={`text-red-600 ${isPending && "animate-ping"}`} />
                                 : <FaRegHeart onClick={onLike.bind(post)} className={isPending && "animate-ping"} />
                         }
                     </button>
-                    <span className="text-xl sm:text-2xl md:text-3xl text-gray-300 block"> {post?.likes?.length > 0 ? post?.likes?.length + " Likes" : "No likes yet"}</span>
+                    <span className="text-xl sm:text-2xl md:text-3xl text-gray-300 block"> {currentPost?.likes?.length > 0 ? currentPost?.likes?.length + " Likes" : "No likes yet"}</span>
                 </div>
                 <div >
                     <button className="text-4xl sm:text-5xl md:text-6xl text-gray-400 cursor-pointer hover:text-blue-200">
@@ -91,13 +92,13 @@ export default function PostCard({ post, setPost, userSession }) {
                 isOpen={isDeleteModalOpen}
                 onClose={() => setIsDeleteModalOpen(false)}
                 onDelete={onDelete.bind(post)}
-                title={post.title} />
+                title={currentPost.title} />
             <EditModal
                 isOpen={isEditModalOpen}
                 onClose={() => setIsEditModalOpen(false)}
                 onSave={onEdit.bind(post)}
-                initialTitle={post.title}
-                initialDescription={post.description}
+                initialTitle={currentPost.title}
+                initialDescription={currentPost.description}
             />
         </motion.div>
     )
