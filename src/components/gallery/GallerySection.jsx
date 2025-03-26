132264dc966f42dbf3e457ef045410affa8dccba
vladinSon <vladinson009@icloud.com>
2025-03-27 00:38:46 +0100

    removed custom hook and used useQuery instead

--- src/components/gallery/GallerySection.jsx
+++ src/components/gallery/GallerySection.jsx
@@ -1,5 +1,4 @@
-import useFetchData from "../../hooks/useFetchData";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import TattooCard from "./TattooCard";
 import Spinner from "../partials/Spinner";
@@ -9,11 +8,23 @@ import galleryService from "../../services/galleryService";
 import useSearchBar from "../../hooks/useSearchBar";
 import SearchBar from "./SearchBar";
 import SubmitFormButton from "../partials/form/SubmitFormButton";
+import { useQuery } from "@tanstack/react-query";
+import context from "../../context/context";
 
 export default function GallerySection() {
+    const { count, setCount } = useContext(context);
     const [pagination, setPagination] = useState({ skip: 0, limit: 8 });
-    const { data, isLoading, error } = useFetchData(galleryService.getGallery, 'getGallery', pagination.skip, pagination.limit);
-    const { query, setQuery, onSearch, filteredData, loadMore, isPending, isMore, onScrollUp, error: searchErr } = useSearchBar(data, pagination, setPagination);
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['getGallery'], queryFn: async () => {
+            const [count, data] = await Promise.all([
+                galleryService.getCountGallery(),
+                galleryService.getGallery(pagination.skip, pagination.limit)
+            ])
+            setCount(count);
+            return data
+        }, staleTime: Infinity
+    })
+    const { query, setQuery, onSearch, filteredData, loadMore, isPending, onScrollUp, error: searchErr } = useSearchBar(data, pagination, setPagination);
 
     // gallery section with all tattoo with possibility for filtering, load more and scroll to top
     if (isLoading) return <Spinner />
@@ -25,7 +36,7 @@ export default function GallerySection() {
                 <div className="container mx-auto px-6 ">
                     <h2 className="text-4xl font-black mb-12 drop-shadow-lg text-center pt-16">Our Tattoo Gallery</h2>
                     <SearchBar onSearch={onSearch} query={query} setQuery={setQuery} />
-                    <p>Results {filteredData?.length} of {data.length}</p>
+                    <p>Results {filteredData?.length} of {count}</p>
                     {filteredData?.length > 0 ? <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]  place-items-center gap-20 md:gap-25 lg:gap-30 px-6">
                         {filteredData.map((tattoo) => (
                             <TattooCard key={tattoo.objectId} tattoo={tattoo} />
@@ -34,7 +45,7 @@ export default function GallerySection() {
                         : <NoContent content="Nothing in Gallery yet..." />}
                 </div>
                 <div className="flex justify-center mt-8">
-                    {isMore
+                    {count > filteredData?.length
                         ? <SubmitFormButton textContent="Load more" isPending={isPending} clickHandler={loadMore} />
                         : <SubmitFormButton textContent="Scroll to top" isPending={isPending} clickHandler={onScrollUp} />
                     }
