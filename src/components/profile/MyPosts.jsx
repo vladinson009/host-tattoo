85c61ec3b400fe4109a3d2e0b3cdff1e4efd4914
vladinSon <vladinson009@icloud.com>
2025-03-27 00:39:36 +0100

    using useQuery for myPosts

--- src/components/profile/MyPosts.jsx
+++ src/components/profile/MyPosts.jsx
@@ -1,16 +1,27 @@
 
 import PostComponent from "../post/PostComponent";
 import postService from '../../services/postService';
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import context from "../../context/context";
 
-async function fetchOwnPosts(setPost, _id, signal) {
-    const posts = await postService.getOwnPosts(signal);
-    const updatedPosts = posts.map((post) => {
-        return { ...post, isLiked: post?.likes?.includes(_id) };
-    });
-    setPost(updatedPosts);
-}
 export default function MyPosts() {
+    const { userSession } = useContext(context);
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['myPosts'],
+        queryFn: async () => {
+            const result = await postService.getOwnPosts();
+            const updatedPosts = result.map((post) => {
+                return { ...post, isLiked: post?.likes?.includes(userSession?._id) };
+            });
+            return updatedPosts;
+        },
+        onSuccess: (data) => {
+            QueryClient.invalidateQueries(['myPosts'], data);
+        }
+    })
+
 
     // show only posts of the current user in My Posts section
-    return <PostComponent fetchPosts={fetchOwnPosts} title="My posts" />
+    return <PostComponent data={data} error={error} isLoading={isLoading} title="My posts" />
 }
