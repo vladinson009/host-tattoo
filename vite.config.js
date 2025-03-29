f65cd9bc4dbb4ffc1e6675991d77cbd0620358a4
vladinSon <vladinson009@icloud.com>
2025-03-29 16:32:26 +0100

    added vitest testing library

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    css: true,
+  },
 });
