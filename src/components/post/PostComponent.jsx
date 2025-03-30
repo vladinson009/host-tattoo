02643e7153270edded1c3103211fb5dd3dcbe1bc
vladinSon <vladinson009@icloud.com>
2025-03-30 21:16:41 +0200

    change color of text to match the whole project

--- src/components/post/PostComponent.jsx
+++ src/components/post/PostComponent.jsx
@@ -15,7 +15,7 @@ export default function PostComponent({ data, error, isLoading, title }) {
         <>
             {globalMessage && <Toast message={globalMessage} type="message" />}
             {error && <Toast message={error} />}
-            <div className="min-h-[calc(100vh-4rem-4rem)] flex flex-col items-center justify-center text-red-600 text-center p-6">
+            <div className="min-h-[calc(100vh-4rem-4rem)] flex flex-col items-center justify-center text-white text-center p-6">
                 <div className="max-w-2xl mx-auto py-8 space-y-6 pt-20">
                     <h2 className="text-4xl font-black mb-6 drop-shadow-lg text-center">{title}</h2>
                     {data?.map((post) => (
